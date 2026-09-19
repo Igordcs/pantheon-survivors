1666c7cd6a657729645c4448cd34c4b4004e2751
airton23 <airtondasf2305@gmail.com>
2026-09-18 21:32:26 -0300

    feat(spawner): distribute enemies by biome, add map wave progression, and integrate animated boss chest

--- scripts/spawning/spawn_director.gd
+++ scripts/spawning/spawn_director.gd
@@ -1,6 +1,6 @@
 extends Node
 class_name SpawnDirector
-## Owns the run clock and applies a controlled, data-driven horde progression.
+## Owns the run clock and applies a controlled, data-driven horde progression per map.
 
 signal time_updated(time_string: String)
 signal wave_started(index: int, wave: WaveData)
@@ -19,7 +19,7 @@ var _progression_paused: bool = false
 
 func _ready() -> void:
 	if waves.is_empty():
-		_generate_default_progression()
+		_setup_map_progression()
 	if not waves.is_empty():
 		_apply_wave(0)
 
@@ -64,7 +64,7 @@ func _apply_wave(index: int) -> void:
 	if is_instance_valid(spawner):
 		spawner.apply_wave_data(wave)
 	wave_started.emit(index, wave)
-	print("Horde phase %d started (cap: %d)." % [index + 1, wave.max_enemies])
+	print("Fase da Horda %d iniciada (Cap: %d)." % [index + 1, wave.max_enemies])
 
 
 func _update_time_ui() -> void:
@@ -72,40 +72,88 @@ func _update_time_ui() -> void:
 	time_updated.emit("%02d:%02d" % [total_seconds / 60, total_seconds % 60])
 
 
-func _generate_default_progression() -> void:
+func _setup_map_progression() -> void:
+	var scene_name := get_tree().current_scene.name.to_lower()
+	
+	if "ice" in scene_name or "snow" in scene_name or "gelo" in scene_name:
+		_generate_ice_progression()
+	elif "wasteland" in scene_name or "desert" in scene_name or "apocalipse" in scene_name:
+		_generate_wasteland_progression()
+	else:
+		_generate_plains_progression()
+
+
+# MAPA 1: PLANÍCIE
+func _generate_plains_progression() -> void:
 	var bat := _entry(&"bat", 1.6, 0.5, 2, 5, 80)
-	var draugr := _entry(&"draugr", 1.4, 1.0, 1, 3, 0)
-	var skeleton := _entry(&"skeleton", 1.2, 0.8, 2, 5, 40)
-	var wolf := _entry(&"wolf", 1.0, 0.7, 3, 6, 30)
-	var harpy := _entry(&"harpy", 0.8, 1.25, 1, 3, 18)
-	var ranged_slime := _entry(&"ranged_slime", 0.6, 1.5, 1, 2, 24)
-	var healer_slime := _entry(&"healer_slime", 0.2, 2.5, 1, 1, 6)
-	var medusa := _entry(&"medusa", 0.35, 3.0, 1, 2, 8)
-	var mummy := _entry(&"mummy", 0.3, 3.0, 1, 2, 10)
-	var cyclops := _entry(&"cyclops", 0.15, 4.0, 1, 1, 10)
-	var orc := _entry(&"orc", 0.12, 5.0, 1, 1, 8)
-	var minotaur := _entry(&"minotaur", 0.04, 8.0, 1, 1, 1)
-	var ammit := _entry(&"ammit", 0.035, 5.5, 1, 1, 6)
-	var valkyrie := _entry(&"corrupted_valkyrie", 0.045, 5.0, 1, 2, 8)
-	var warlock := _entry(&"warlock", 0.45, 2.5, 1, 2, 12)
-	var lizard := _entry(&"lizard", 0.7, 1.6, 2, 4, 25)
-	var imp := _entry(&"imp", 0.5, 2.0, 2, 4, 15)
+	var lizard := _entry(&"lizard", 1.2, 1.2, 2, 4, 35)
+	var ranged_slime := _entry(&"ranged_slime", 0.8, 1.5, 1, 3, 24)
+	var healer_slime := _entry(&"healer_slime", 0.3, 2.0, 1, 2, 8)
+	var cyclops := _entry(&"cyclops", 0.2, 4.0, 1, 1, 10)
+	var minotaur := _entry(&"minotaur", 0.05, 8.0, 1, 1, 2)
 
 	waves = [
-		_wave(60.0, 0.8, 25, 1.2, 4, [bat, draugr]),
-		_wave(60.0, 0.7, 40, 1.8, 5, [bat, draugr, harpy, skeleton, wolf, lizard]),
-		_wave(60.0, 0.6, 55, 2.6, 6, [bat, draugr, harpy, ranged_slime, skeleton, wolf, lizard]),
-		_wave(120.0, 0.55, 80, 3.8, 7, [draugr, harpy, ranged_slime, healer_slime, medusa, warlock, lizard]),
-		_wave(90.0, 0.45, 110, 5.2, 8, [draugr, harpy, ranged_slime, healer_slime, medusa, mummy, cyclops, ammit, skeleton, lizard, imp]),
-		_wave(90.0, 0.35, 150, 7.0, 10, [harpy, ranged_slime, healer_slime, medusa, mummy, cyclops, orc, ammit, valkyrie, warlock, lizard, imp]),
-		_wave(120.0, 0.25, 220, 10.0, 12, [bat, draugr, harpy, ranged_slime, healer_slime, medusa, mummy, cyclops, orc, minotaur, ammit, valkyrie, skeleton, wolf, warlock, lizard, imp]),
+		_wave(60.0, 0.8, 25, 1.2, 4, [bat]),
+		_wave(60.0, 0.7, 40, 1.8, 5, [bat, lizard]),
+		_wave(60.0, 0.6, 60, 2.6, 6, [bat, lizard, ranged_slime]),
+		_wave(90.0, 0.55, 85, 3.8, 7, [lizard, ranged_slime, healer_slime]),
+		_wave(90.0, 0.45, 120, 5.5, 8, [cyclops, ranged_slime, healer_slime]),
+		_wave(120.0, 0.3, 180, 8.5, 10, [cyclops, minotaur, lizard, bat]),
 	]
 
 	horde_events = [
-		_event(90.0, bat, 30, "A swarm of bats approaches!"),
-		_event(150.0, ranged_slime, 12, "Ranged slimes surround the battlefield!"),
-		_event(450.0, minotaur, 1, "A Minotaur has entered the horde!"),
-		_event(540.0, bat, 50, "The final horde is gathering!"),
+		_event(70.0, bat, 35, "Um bando imenso de morcegos aproxima-se!"),
+		_event(160.0, ranged_slime, 16, "Slimes arcanos cercam o campo!"),
+		_event(380.0, minotaur, 2, "Minotauros avançam enfurecidos!"),
+	]
+
+
+# MAPA 2: RUÍNAS / TERRA DEVASTADA
+func _generate_wasteland_progression() -> void:
+	var skeleton := _entry(&"skeleton", 1.5, 0.8, 2, 5, 60)
+	var imp := _entry(&"imp", 1.2, 1.4, 2, 4, 30)
+	var warlock := _entry(&"warlock", 0.6, 2.2, 1, 2, 14)
+	var medusa := _entry(&"medusa", 0.4, 3.0, 1, 2, 10)
+	var mummy := _entry(&"mummy", 0.4, 3.2, 1, 2, 12)
+	var orc := _entry(&"orc", 0.2, 4.5, 1, 2, 10)
+	var ammit := _entry(&"ammit", 0.05, 7.5, 1, 1, 3)
+
+	waves = [
+		_wave(60.0, 0.8, 30, 1.3, 4, [skeleton]),
+		_wave(60.0, 0.7, 45, 2.0, 5, [skeleton, imp]),
+		_wave(60.0, 0.6, 65, 2.8, 6, [skeleton, imp, warlock]),
+		_wave(90.0, 0.5, 90, 4.2, 7, [imp, warlock, mummy, medusa]),
+		_wave(90.0, 0.4, 130, 6.0, 9, [warlock, mummy, medusa, orc]),
+		_wave(120.0, 0.28, 200, 9.5, 12, [skeleton, imp, orc, ammit, medusa]),
+	]
+
+	horde_events = [
+		_event(80.0, imp, 25, "Diabretes sobrevoam a área em chamas!"),
+		_event(170.0, skeleton, 40, "A horda de esqueletos ergue-se do solo!"),
+		_event(390.0, ammit, 2, "Ammit surge para devorar os indignos!"),
+	]
+
+
+# MAPA 3: GÉLIDO
+func _generate_ice_progression() -> void:
+	var wolf := _entry(&"wolf", 1.6, 0.7, 3, 6, 65)
+	var draugr := _entry(&"draugr", 1.3, 1.1, 2, 4, 50)
+	var harpy := _entry(&"harpy", 0.8, 1.8, 1, 3, 22)
+	var valkyrie := _entry(&"corrupted_valkyrie", 0.08, 6.0, 1, 2, 6)
+
+	waves = [
+		_wave(60.0, 0.75, 30, 1.4, 5, [wolf]),
+		_wave(60.0, 0.65, 50, 2.2, 6, [wolf]),
+		_wave(60.0, 0.55, 75, 3.2, 7, [wolf, draugr]),
+		_wave(90.0, 0.45, 105, 4.8, 8, [draugr, harpy, wolf]),
+		_wave(90.0, 0.35, 145, 7.0, 10, [draugr, harpy, valkyrie]),
+		_wave(120.0, 0.25, 220, 10.5, 13, [wolf, draugr, harpy, valkyrie]),
+	]
+
+	horde_events = [
+		_event(75.0, wolf, 30, "Uma matilha implacável de lobos ataca!"),
+		_event(180.0, draugr, 35, "Guerreiros Draugr emergem da nevasca!"),
+		_event(420.0, valkyrie, 3, "Valquírias Corrompidas descem dos céus!"),
 	]
 
 
